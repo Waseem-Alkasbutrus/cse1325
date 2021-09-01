07c29de82d8944ec6cc262e55e3036e308e3d59b
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-09-01 18:53:43 -0500

    Added Roller.java to P02/full_credit

---
+++ P02/full_credit/Roller.java
@@ -0,0 +1,24 @@
+public class Roller {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("usage: java Roller [#dice] [#sides]");
+
+            System.exit(1);
+        }
+
+        int numOfDice = Integer.parseInt(args[0]);
+        int numOfSides = Integer.parseInt(args[1]);
+        int sumOfRolls = 0;
+
+        int[] diceRolls = new int[numOfDice];
+
+        for (int i = 0; i < numOfDice; i++) {
+            diceRolls[i] = (int) (Math.random() * numOfSides) + 1;
+            sumOfRolls += diceRolls[i];
+
+            System.out.printf("%d ", diceRolls[i]);
+        }
+
+        System.out.printf("|| %d %.2f\n", sumOfRolls,  ((float) sumOfRolls/ (float) numOfDice));
+    }
+}
