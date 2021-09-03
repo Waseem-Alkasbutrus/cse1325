61d587d507fa53bfe58f3f22ec31869ef4000667
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-09-02 22:00:39 -0500

    Made changes to P02/extreme_bonus/Formula80.java

--- P02/extreme_bonus/Formula80.java
+++ P02/extreme_bonus/Formula80.java
@@ -11,49 +11,59 @@ public class Formula80 {
             System.out.println("Ready...Steady..GO! 🏁");
         }
 
-        Die die = new Die(6);
+        Die die = new Die(4);
+
         Player player1 = new Player(args[0]);
         Player player2 = new Player(args[1]);
+        
+        //Player[] players = {new Player(args[0]), new Player(args[1])};
         boolean turnOfPlayer1 = true;
         int winner = 0;
-
+        int roundNumber = 0;
         while(winner == 0) {
-            //Roll 2 dice
-            int rollTotal = die.roll();
-            rollTotal += die.roll();
+            for (int i = 0; i < 2; i++) {
+                //Roll 2 dice
+                int rollTotal = die.roll();
+                rollTotal += die.roll();
 
-            //if double 0 playerX can pick an option
-            if (rollTotal == 0) {
+                //if double 0 playerX can pick an option
+                if (rollTotal == 0) {
 
-            //if double 6 playerX can pick an option
-            } else if (rollTotal == 12) {
+                //if double 4 playerX can pick an option
+                } else if (rollTotal == 8) {
 
-            //else increment playerX normally
-            } else {
-                if (turnOfPlayer1) {
-                    player1.incrementScore(rollTotal);
-
-                    turnOfPlayer1 = false;
+                //else increment playerX normally
                 } else {
-                    player2.incrementScore(rollTotal);
 
-                    turnOfPlayer1 = true;
+                }
+                    if (turnOfPlayer1) {
+                        player1.incrementScore(rollTotal);
+
+                        turnOfPlayer1 = false;
+                    } else {
+                        player2.incrementScore(rollTotal);
+
+                        turnOfPlayer1 = true;
+                    }
+
+                if (player1.getScore() >= 80) {
+                    winner = 1;
+                } else if (player1.getScore() >= 80) {
+                    winner = 2;
                 }
             }
+            
+            roundNumber++;
 
-            if (player1.getScore() >= 80) {
-                winner = 1;
-            } else if (player1.getScore() >= 80) {
-                winner = 2;
-            }
-        
+            System.out.println("===========================================================");
+            System.out.println("Round " + roundNumber);
             printBoard(player1.getName(), player1.getScore());
             printBoard(player2.getName(), player2.getScore());
         }
     }
 
     private static void printBoard(String playerName, int progress) {
-        System.out.printf("\n\n%s: %d/80\n", playerName, progress);
+        System.out.printf("\n%s: %d/80\n", playerName, progress);
         for(int i = 0; i < 80; i++) {
             if (i <= progress && progress != 0) {
                 System.out.printf("🟩");
