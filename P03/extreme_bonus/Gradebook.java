09eb6950c38b990d71db3b2d100b363b64058379
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-09-12 14:11:58 -0500

    Worked on P03/extreme_bonus

--- P03/extreme_bonus/Gradebook.java
+++ P03/extreme_bonus/Gradebook.java
@@ -1,12 +1,93 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gradebook {
     public static void main(String[] args) {
-        Student waseem = new Student("Waseem");
-        Student matthew = new Student("Matthew Polus");
-        matthew.addExam(59);
-        matthew.addExam(100);
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        Menu menu = new Menu("Select one of the following:", 4);
 
-        System.out.println(waseem + "\n" + matthew);
+        menu.appendOption("Add new students");
+        menu.appendOption("Add exam grades");
+        menu.appendOption("List all averages");
+        menu.appendOption("Exit");
+
+        while (true) {
+            int menuSelection = menu.input();
+            
+            switch (menuSelection) {
+                case 1:
+                    addNewStudents(studentList);
+                    break;
+                case 2:
+                    if (studentList.size() == 0) {
+                        System.out.println("\nNo students have been added yet. Try adding some first\n");
+                    } else {
+                        addNewGrades(studentList);
+                    }
+                    break;
+                case 3: 
+                    if (studentList.size() == 0) {
+                        System.out.println("\nNo students have been added yet. Try adding some first\n");
+                    } else {
+                        listAllAverages(studentList);
+                    }
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default: 
+                    System.err.println("Invalid return from Menu.input()");
+                    System.exit(1);
+            }
+        }
+    }
+
+    private static void addNewStudents(ArrayList<Student> studentList) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println();
+        
+        while (true) {
+            System.out.printf("Enter new student's name\n>> ");
+            String studentNameInput = scan.nextLine();
+            
+            if (studentNameInput.equals("")) {
+                break;
+            } else {
+                studentList.add(new Student(studentNameInput));
+            }
+        }
+
+        System.out.println();
+    }
+
+    private static void addNewGrades(ArrayList<Student> studentList) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println();
+
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.printf("Enter %s's grade\n>> ", studentList.get(i).getName());
+            double studentGradeInput = 0;
+            try {
+                studentGradeInput = scan.nextDouble();
+            } catch (Exception e) {
+                System.err.println("Invalid double. Please try again.");
+                i--;
+                continue;
+            }
+            
+            studentList.get(i).addExam(studentGradeInput);
+        }
+
+        System.out.println();
+    }
+
+    private static void listAllAverages(ArrayList<Student> studentList) {
+        System.out.printf("\n%15s%10s\n", "Student Name", "Average");
+        for(int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+        System.out.println();
     }
 }
