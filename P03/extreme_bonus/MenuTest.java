5d7ca88f6b921c99c739cdf91c4dd0179328a47f
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-09-12 11:20:01 -0500

    Reusing MenuTest.java as a regression test file for the Menu class

--- P03/extreme_bonus/MenuTest.java
+++ P03/extreme_bonus/MenuTest.java
@@ -1,17 +1,36 @@
 public class MenuTest{
     public static void main(String[] args) {
-        Menu startMenu = new Menu("Select an option below:", 3);
-        startMenu.appendOption("PLAY");
-        startMenu.appendOption("SETTINGS");
-        startMenu.appendOption("EXIT");
+        Menu menu = new Menu("Select an option below:", 2);
+        int numOfFailedTests = 0;
 
-        startMenu.printMenu();
+        //TEST 1: Verify the prompt provided to the constructor is correctly returned by the getPrompt() method
+        final String menuPrompt = menu.getPrompt();
 
-        System.out.println();
+        if (menuPrompt != "Select an option below:") {
+            numOfFailedTests++;
 
-        startMenu.replaceOptionAt(1, "CREDITS");
-        startMenu.addOptionAt(2, "Options");
+            System.out.println("\nTEST 1 FAILED: getPrompt() method did not return prompt correctly.");
+            System.out.println("\tEXPECTED: \"Select an option below:\"");
+            System.out.println("\tRETURNED: \"" + menuPrompt + "\"");
+        }
 
-        startMenu.input();
+        //TEST 2: Verify that the options are properly returned by the  getOptions() method
+        menu.appendOption("OPTION 1");
+        menu.appendOption("OPTION 2");
+        final String menuOptions = menu.getOptions();
+        
+        if (!menuOptions.equals("[OPTION 1, OPTION 2]")) {
+            numOfFailedTests++;
+
+            System.out.println("\nTEST 2 FAILED: getOptions() method did not return options correctly.");
+            System.out.println("\tEXPECTED: \"[OPTION 1, OPTION 2]\"");
+            System.out.println("\tRETURNED: \"" + menuOptions + "\"");
+        }
+
+        if (numOfFailedTests > 0) {
+            System.out.println("\n" + numOfFailedTests + " TESTS FAILED");
+
+            System.exit(1);
+        }
     }
 }
