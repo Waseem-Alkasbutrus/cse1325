5dcaaba798c6b6c203c1a8fb5f92af4ddcff9f3c
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-09-19 22:18:47 -0500

    P04/bonus/Puzzle.java: guesses attribute now has a bool value for ascii chars from 32 to 126 instead of for each char in the solution

--- P04/bonus/Puzzle.java
+++ P04/bonus/Puzzle.java
@@ -1,23 +1,14 @@
 public class Puzzle {
     private final String solution;
-    private boolean[] guesses;
+    private boolean[] guesses = new boolean[93];
 
     public Puzzle (String solution) {
         this.solution = solution.toLowerCase();
-        this.guesses = new boolean[this.solution.length()];
     }
 
     public boolean guess(char c) {
-        if (this.solution.contains(Character.toString(c).toLowerCase())) {
-            for (int i = 0; i < this.solution.length(); i++) {
-                if (c == this.solution.charAt(i)) {
-                    this.guesses[i] = true;
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
+        this.guesses[c - 32] = this.solution.contains(Character.toString(c).toLowerCase());
+        return this.guesses[c - 32];
     }
 
     public boolean solve(String proposedSolution) {
@@ -33,7 +24,7 @@ public class Puzzle {
         String hiddenSolution = "";
 
         for (int i = 0; i < this.solution.length(); i++) {
-            if (this.guesses[i] || this.solution.charAt(i) == ' ') {
+            if (this.guesses[this.solution.charAt(i) - 32] || this.solution.charAt(i) == ' ') {
                 hiddenSolution = hiddenSolution + Character.toString(this.solution.charAt(i));
             } else {
                 hiddenSolution = hiddenSolution + "_";
