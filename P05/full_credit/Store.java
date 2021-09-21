25733779ef07afe26e5f9fdac72d4f0565c078c1
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2021-09-21 12:34:25 -0500

    P05/full_credit/Store.java: Added ArrayList with 3 taxed, and 3 taxfree items

--- P05/full_credit/Store.java
+++ P05/full_credit/Store.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
 public class Store {
+    //TODO: ask about absense of constructor
+
     public static void main (String[] args) {
         Store store = new Store();
     }
 
     public void cli() {
+        ArrayList<Product> products = new ArrayList () {{
+            add new Taxfree("Milk", 3.33);
+            add new Taxfree("Eggs", 2.33);
+            add new Taxfree("Bread", 2.48);
+            add new Taxed("Ben & Jerry's", 4.38);
+            add new Taxed("DumDums", 3.64);
+            add new Taxed("Skittles", 2.28)
+        }};
+
         
     }
 }
