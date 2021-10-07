60a56e77e866bbd823f80f2133dff5ba3961a69e
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-10-07 01:05:41 -0500

    P06/full_credit/Donut.java: Added Donut.java

---
+++ P06/full_credit/Donut.java
@@ -0,0 +1,20 @@
+public class Donut extends Product {
+    protected Frosting frosting;
+    protected boolean sprinkles;
+    protected Filling filling;
+
+    public Donut(String name, double price, double cost, Frosting frosting, boolean sprinkles, Filling filling) throws IllegalArgumentException {
+        if (frosting == unfrosted && sprinkles) {
+            throw new IllegalArgumentException("Cannot add sprinkles on an unfrosted donut");
+        } 
+        super(name, price, cost);
+        this.frosting = frosting;
+        this.sprinkles = sprinkles;
+        this.filling = filling;
+    }
+
+    @Override
+    public String toString() {
+        return 
+    }
+}
