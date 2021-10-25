95cde4e17a8f07903d23ea961e321eb404aa4c0f
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2021-10-24 19:36:54 -0500

    P08/store/Product.java: Added a Product(BufferedReader) constructor and a save(BufferedWriter) method

--- P08/store/Product.java
+++ P08/store/Product.java
@@ -1,5 +1,10 @@
 package store;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Product {
     protected String name;
     protected double price;
@@ -11,8 +16,20 @@ public class Product {
         this.cost = cost;
     }
 
+    public Product(BufferedReader bufferedReader) throws IOException {
+        this.name = bufferedReader.readLine();
+        this.price = Double.parseDouble(bufferedReader.readLine());
+        this.cost = Double.parseDouble(bufferedReader.readLine());
+    }
+
     public String name() {
         return this.name;
+    }
+
+    public void save(FileWriter fileWriter) throws IOException {
+        fileWriter.write(this.name + '\n');
+        fileWriter.write(Double.toString(this.price) + '\n');
+        fileWriter.write(Double.toString(this.cost) + '\n');
     }
 
     @Override
