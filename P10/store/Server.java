94f6a742feae4450e830fe5105b3558a54f124f6
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2021-11-09 19:49:05 -0600

    Server.java Created Server class

---
+++ P10/store/Server.java
@@ -0,0 +1,25 @@
+package store;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Server extends Person {
+    String ssn;
+
+    public Server(String name, String phone, String ssn) {
+        super(name, phone);
+        this.ssn = ssn;
+    }
+
+    public Server(BufferedReader bufferedReader) throws IOException {
+        super(bufferedReader);
+        this.ssn = bufferedReader.readLine();
+    }
+
+    @Override
+    public void save(BufferedWriter bufferedWriter) throws IOException {
+        super.save(bufferedWriter);
+        bufferedWriter.write(this.ssn + '\n');
+    }
+}
